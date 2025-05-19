376af415f78a88232507ee564368e543f6f442c8
Léopold Rolland <leopold.rolland@hotmail.fr>
2025-05-19 16:50:44 +0200

    Header : LR non cliquable

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,19 +3,11 @@ import Navbar from '../Navbar/Navbar'
 import './Header.css'
 
 const Header = () => {
-  const { homepage, title } = header
+  const { title } = header
 
   return (
     <header className='header center'>
-      <h3>
-        {homepage ? (
-          <a href={homepage} className='link'>
-            {title}
-          </a>
-        ) : (
-          title
-        )}
-      </h3>
+      <h3>{title}</h3>
       <Navbar />
     </header>
   )
